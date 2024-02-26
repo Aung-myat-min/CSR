dd62130aee4fb7761741c4d4a1366a1831c76c7f
PhyoMinKhant-Xem <phyominkhant.xem@gmail.com>
2024-02-26 14:51:05 +0630

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import ThemeSwitch from "./themes/ThemeSwitch"; // Adjust the path based on your
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/images/blue_csr_logo.png";
-import logo2 from "@/public/images/white_csr_logo.png";
 import { useState } from "react";
+import { RiMenu4Line } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
 
 export default function Home() {
   let Links = [
@@ -16,7 +17,7 @@ export default function Home() {
   let [open, setOpen] = useState(false);
   return (
     <nav className="fixed w-full top-0 left-0">
-      <div className="flex justify-between items-center w-11/12 h-20  rounded-xl m-auto mt-5 bg-primary dark:bg-secondary relative">
+      <div className="flex justify-between items-center w-11/12 lg:h-20 md:h-16 h-14  rounded-xl m-auto mt-5 bg-primary dark:bg-secondary relative">
         <Image
           className="ms-5"
           src={logo}
@@ -27,15 +28,12 @@ export default function Home() {
           priority={false}
           title="Loading Light/Dark Toggle"
         />
-        <div
-          onClick={() => setOpen(!open)}
-          className="fixed right-8 top-6 cursor-pointer md:hidden w-20 h-20"
-        >
-          {open ? <Image src={logo2} alt="" /> : <Image src={logo} alt="" />}
-        </div>
+
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-20 bg-gray-300" : "top-[-490px]"
+          className={`md:flex md:items-center md:pb-0  absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open
+              ? "top-20 bg-primary dark:bg-secondary rounded-xl"
+              : "top-[-490px]"
           }`}
         >
           {Links.map((link, index) => (
@@ -48,10 +46,18 @@ export default function Home() {
               </Link>
             </li>
           ))}
+        </ul>
+        <div className="flex">
+          <div
+            onClick={() => setOpen(!open)}
+            className=" items-center flex cursor-pointer md:hidden"
+          >
+            {open ? <RxCross2 /> : <RiMenu4Line />}
+          </div>
           <div className="flex items-center me-5 md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static cursor-pointer">
             <ThemeSwitch />
           </div>
-        </ul>
+        </div>
       </div>
     </nav>
   );
