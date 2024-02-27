78d6e87ca2d71a2d45a1fb13b89a836357670001
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-02-28 00:30:55 +0630

    created item component for event page
    configured next config file for accepting images from pinterest route and created a new component called 'ItemShowCase'

---
+++ components/itemShowCase.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function ItemShowCase(props: {
+  header: string;
+  description: string;
+  url: string;
+  id: number;
+}) {
+  return (
+    <div className="w-11/12 m-auto">
+      <div className="grid grid-cols-3 gap-10">
+        <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-3xl">
+          <div className="">
+            <Image
+              src={props.url}
+              alt={props.header}
+              priority={false}
+              title={props.header}
+              width={560}
+              height={315}
+            />
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+            <h1 className="font-dmserif text-3xl font-bold text-white">
+              {props.header}
+            </h1>
+            <p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+              {props.description}
+            </p>
+            <Link
+              href={`/event/${props.id}`}
+              className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60"
+            >
+              See More
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
