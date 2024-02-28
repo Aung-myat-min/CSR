11030dca7f93722db3c83289d648d6aa1c3f25c9
PhyoMinKhant-Xem <phyominkhant.xem@gmail.com>
2024-02-28 09:49:52 +0630

    finish item components for events page

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,26 @@ const items = [
   {
     headers: "Car",
     description: "This is a car",
-    url: "https://i.pinimg.com/236x/1f/e9/c3/1fe9c3561790f500d4e6151911198389.jpg",
+    url: "https://wallpaperbat.com/img/287163-anime-desktop-wallpaper-top-free-anime-desktop-background.jpg",
     id: 1,
   },
   {
     headers: "Cat",
     description: "This is a Cat",
-    url: "https://i.pinimg.com/236x/16/ca/b1/16cab153397fc070d5369635ba891e8d.jpg",
+    url: "https://wallpaperbat.com/img/864500-sung-jin-woo-anime-art-wallpaper-cool-anime-wallpaper-hd-anime-wallpaper-anime-wallpaper.jpg",
     id: 1,
   },
   {
     headers: "Dog",
     description: "This is a Dog",
-    url: "https://i.pinimg.com/236x/9e/bf/a9/9ebfa99ee766bd67f6d3101778748504.jpg",
+    url: "https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg",
     id: 1,
   },
 ];
 
 export default function App() {
   return (
-    <div className="grid grid-cols-1 gap-10">
+    <div className="grid grid-cols-3 gap-10">
       {items.map((item, index) => {
         return (
           <ItemShowCase
