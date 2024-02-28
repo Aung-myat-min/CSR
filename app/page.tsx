b70c4d793441649ea679e6901291b78cac90da23
PhyoMinKhant-Xem <phyominkhant.xem@gmail.com>
2024-02-28 15:46:18 +0630

    Event Details Page

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <div className="">
       <nav className="fixed w-full top-0 left-0">
-        <div className="flex justify-between items-center w-11/12 lg:h-20 md:h-16 h-14  rounded-xl m-auto mt-3 relative">
+        <div className="flex justify-between items-center w-11/12 lg:h-20 md:h-16 h-14 bg-white  rounded-xl m-auto mt-3 relative">
           <div className="flex items-center lg:ms-5 md:ms-3 font-semibold px-3 py-1 rounded duration-500 md:static cursor-pointer">
             <FaChevronLeft />
           </div>
@@ -26,7 +26,7 @@ export default function App() {
           DAY DONATION
         </h1>
       </div>
-      <div className="max-w-[600px] lg:h-[380px] md:h-[300px] xl:h-[420px] h-[250px] w-11/12 m-auto  flex items-center relative group">
+      <div className="max-w-[600px] h-fit w-11/12 m-auto  flex items-center relative group my-10">
         <Image
           className="rounded-2xl"
           src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
@@ -45,6 +45,66 @@ export default function App() {
           odit voluptatem aspernatur, natus, corporis cupiditate, labore unde
           vitae porro.
         </p>
+      </div>
+      <div className="w-11/12 m-auto">
+        <h2 className="font-bold text-2xl mb-2">More Photo</h2>
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-5 mt-5">
+          <div className="max-w-[600px] h-fit ">
+            <Image
+              className="rounded-2xl"
+              src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
+              alt="Loading Light/Dark Toggle"
+              priority={false}
+              title="Loading Light/Dark Toggle"
+              width={1920}
+              height={1080}
+            />
+          </div>
+          <div className="max-w-[600px] h-fit ">
+            <Image
+              className="rounded-2xl"
+              src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
+              alt="Loading Light/Dark Toggle"
+              priority={false}
+              title="Loading Light/Dark Toggle"
+              width={1920}
+              height={1080}
+            />
+          </div>
+          <div className="max-w-[600px] h-fit ">
+            <Image
+              className="rounded-2xl"
+              src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
+              alt="Loading Light/Dark Toggle"
+              priority={false}
+              title="Loading Light/Dark Toggle"
+              width={1920}
+              height={1080}
+            />
+          </div>
+          <div className="max-w-[600px] h-fit ">
+            <Image
+              className="rounded-2xl"
+              src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
+              alt="Loading Light/Dark Toggle"
+              priority={false}
+              title="Loading Light/Dark Toggle"
+              width={1920}
+              height={1080}
+            />
+          </div>
+          <div className="max-w-[600px] h-fit ">
+            <Image
+              className="rounded-2xl"
+              src="https://wallpaperbat.com/img/864499-anime-wallpaper-hd-1920x1080.jpg"
+              alt="Loading Light/Dark Toggle"
+              priority={false}
+              title="Loading Light/Dark Toggle"
+              width={1920}
+              height={1080}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
