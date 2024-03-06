52099e54db659278be242cf5a1fe9f9de88c8c53
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-03-07 01:57:21 +0630

    edited the api routes

--- app/api/events/upcoming/route.ts
+++ app/api/events/upcoming/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   await connectMongo();
   try {
-    const events = await EventModel.find({ Completed: true });
+    const events = await EventModel.find({ Completed: false });
     return NextResponse.json(events);
   } catch (error) {
     return NextResponse.json({ error: error });
