8037228528db172565f1777a59e8f51f76161f47
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-08-07 17:10:07 +0630

    added: middleware for admin dashboard

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: [
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
+
+export default function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  
+  // Get hostname of request (e.g. admin.localhost:3000)
+  const hostname = req.headers.get("host")!;
+
+  const searchParams = req.nextUrl.searchParams.toString();
+  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
+
+  // Handling for `admin.localhost:3000` domain
+  if (hostname === `admin.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    return NextResponse.rewrite(
+      new URL(`/admin${path === "/" ? "" : path}`, req.url),
+    );
+  }
+
+  // Allow requests to pass through for other domains
+  return NextResponse.next();
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,20 @@
-import MillionLint from '@million/lint';
+import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "6d5qbqxnwl8juodu.public.blob.vercel-storage.com"
-    }, {
-      protocol: "https",
-      hostname: "96quhetnznzbwqlu.public.blob.vercel-storage.com"
-    }]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "6d5qbqxnwl8juodu.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "96quhetnznzbwqlu.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 export default MillionLint.next({
-  rsc: true
+  rsc: true,
 })(nextConfig);
